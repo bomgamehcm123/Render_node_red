65e486cd6f58eb5af1c3d8d29b89453ef04fe3cf
bomgamehcm123 <nknnguyen03@gmail.com>
2025-08-08 12:33:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,16 @@ module.exports = {
     uiPort: process.env.PORT || 1880,
 
     // Để bảo mật nếu bạn muốn thêm sau
-    // adminAuth: null,
+ adminAuth: {
+        type: "credentials",
+        users: [
+            {
+                username: "UTE21146282",
+                password: "$2y$08$Zi46aTUTpUJwIF0p.HldGeJMAwkT3fwRQiZ6lDONfWNfERwDAczB6",
+permissions: "*"
+            }
+        ]
+    },
 
     functionGlobalContext: {}, // Cho phép dùng biến toàn cục
 
